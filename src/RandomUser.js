8aef475baa41dde877c2e07c3b046b8cd60b74af
Van Nguyen <vanthoai@fb.com>
2022-12-09 15:18:36 +0000

    debugged th app. cleaned up setinterval and called fetch function

--- src/RandomUser.js
+++ src/RandomUser.js
@@ -1,67 +1,71 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import User from './User';
+import User from "./User";
 
-const colors = ['#0c9bbd', 'red', 'orange', 'green'];
+const colors = ["#0c9bbd", "red", "orange", "green"];
 
 const RandomUserTwo = () => {
   const [num, setNum] = useState(0);
-  const [searchChange, setSearchChange] = useState('');
+  const [searchChange, setSearchChange] = useState("");
   const [searchWord, setSearchWord] = useState(
-    localStorage.setItem('user') || 'foobar'
+    localStorage.getItem("user") || "foobar"
   );
-  
+
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const fetchUser = () => {
+    const fetchUser = async () => {
       const res = await fetch(`https://randomuser.me/api/?seed=${searchWord}`);
       const data = await res.json();
       setData(data.results);
     };
-  }, []);
+
+    fetchUser();
+  }, [searchWord]);
 
   useEffect(() => {
-    localStorage.getItem('user', searchWord);
+    localStorage.setItem("user", searchWord);
   }, [searchWord]);
 
   useEffect(() => {
     const colorInterval = setInterval(() => {
-      console.log('i am running');
+      console.log("i am running");
       setNum((prevNum) => (prevNum === 3 ? 0 : prevNum + 1));
     }, 7000);
+
+    return () => clearInterval(colorInterval);
   }, []);
 
   return (
     <div
       style={{
         backgroundColor: colors[num],
-        transition: 'background-color 4s'
+        transition: "background-color 4s",
       }}
-      className='container'
+      className="container"
     >
-      <div className='person'>
+      <div className="person">
         {data?.map((data) => (
           <User key={data.id.value} data={data} />
         ))}
       </div>
-      <div className='form-wrapper'>
+      <div className="form-wrapper">
         <form
           onSubmit={(e) => {
             e.preventDefault();
             setSearchWord(searchChange);
-            setSearchChange('');
+            setSearchChange("");
           }}
         >
-          <label htmlFor='search'>Search:</label>
+          <label htmlFor="search">Search:</label>
           <input
-            id='search'
+            id="search"
             onChange={(e) => setSearchChange(e.target.value)}
             value={searchChange}
-            name='searchWord'
-            placeholder='Username'
+            name="searchWord"
+            placeholder="Username"
           />
-          <button type='submit'>Submit</button>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
